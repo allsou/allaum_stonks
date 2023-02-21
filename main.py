96109c4c3f6cb5082c9132c322bed0bf8d2f2c6d
Allan Santos <allansantos@luizalabs.com>
2023-02-20 23:20:04 -0300

    feat: processing time

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import asyncio
+import time
+
 from assets import ASSETS
 from services.price import Price
 from settings.base import LOGGER
 
 
 def main():
+    start_at = time.time()
     LOGGER.info('Starting allaum assets to buy')
     price = Price()
-    price.get_assets_to_buy(ASSETS)
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(
+        price.get_assets_to_buy(ASSETS)
+    )
+    LOGGER.info(f'Took {time.time() - start_at}')
 
 
 if __name__ == '__main__':
